2005e0650e9acfcbf660b288610bca886b7c5021
ruslan.yusufov@vimmi.net <ruslan.yusufov@vimmi.net>
2021-06-09 20:13:08 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { exec } = require('child_process');
 
-const stdout = exec(`npm run my_test && echo ${process.env.npm_config_sha}`);
+// const stdout = exec(`npm run my_test && echo ${process.env.npm_config_sha}`);
 
-console.log('SHA: ', process.env.npm_config_sha);
-console.log('stdout', Buffer.from(stdout).toString());
+// console.log('SHA: ', process.env.npm_config_sha);
+// console.log('stdout', Buffer.from(stdout).toString());
+// const stdout =
+exec('npm config set test=567576 && npm run get', function (
+    error,
+    stdout,
+    stderr
+) {
+    console.log('stdout', Buffer.from(stdout).toString());
+});
