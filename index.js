1c39ded4c472369d85963c17c450b4714d920124
ruslan.yusufov@vimmi.net <ruslan.yusufov@vimmi.net>
2021-06-09 19:04:33 +0300

    test

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function test(params) {
-                          console.log("param", params)
-}
+const { execSync } = require('child_process');
+
+const stdout = execSync(`npm run my_test`);
+
+console.log('SHA: ', process.env.npm_config_sha);
+console.log('stdout', Buffer.from(stdout).toString());
