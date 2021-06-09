9164c00c94a0430dcc27b79eb0a8922d67a93bb9
ruslan.yusufov@vimmi.net <ruslan.yusufov@vimmi.net>
2021-06-09 19:51:28 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { execSync } = require('child_process');
 
-const stdout = execSync(`npm run my_test`);
+const stdout = execSync(
+    `npm run my_test && echo ${process.env.npm_config_sha}`
+);
 
 console.log('SHA: ', process.env.npm_config_sha);
 console.log('stdout', Buffer.from(stdout).toString());
